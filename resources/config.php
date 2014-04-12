5f8b0ac66cae185b96d956816b0d5f887190bc41
Tsenmu <i@tsenmu.com>
2014-04-12 17:21:48 +0800

    Modified config.php to corret database entry

--- resources/config.php
+++ resources/config.php
@@ -1,36 +1,33 @@
 <?php
 $config = array(
     "db" => array(
-      "db1" => array(
-          "dbname" => "database1"
-          "username" => "dbUser"
-        ),
-      "db2" => array(
-          "dbname" => "database2"
-        )
+        "dbname" => "projectionist"
+        "username" => "root"
+        "password" => "123qwe123"
+        "host" => "vps.tsenmu.com"
       ),
     "urls" => array(
-      "baseUrl" => "http://example.com"
+      "baseUrl" => "localhost"
       ),
     "paths" => array(
-      "resources" => "/path/to/resources",
-      "images" => array(
-          "content" => $_SERVER["DOCUMENT_ROOT"]. "/images/content",
-          "layout" => $_SERVER["DOCUMENT_ROOT"]."/images/layout"
+        "resources" => "/path/to/resources",
+        "images" => array(
+            "content" => $_SERVER["DOCUMENT_ROOT"]. "/images/content",
+            "layout" => $_SERVER["DOCUMENT_ROOT"]."/images/layout"
         )
-      )
-    );
+    )
+);
 
 
-    defined("LIBRARY_PATH")
-      or define("LIBRARY_PATH",realpath(dirname(_FILE_).'/library'));
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH",realpath(dirname(_FILE_).'/library'));
 
-    defined("TEMPLATES_PATH")
-      or define("TEMPLATES_PATH",realpath(dirname(_FILE).'/templates'));
+defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH",realpath(dirname(_FILE).'/templates'));
 
-    ini_set("error_reporting",true);
-    error_reporting(E_ALL|E_STRCT);
+ini_set("error_reporting",true);
+error_reporting(E_ALL|E_STRCT);
 
 
- ?>
+?>
     
