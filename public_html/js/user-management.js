c2450f9b79f30417ce1e7f5443c98cf51b18e800
Tsenmu <i@tsenmu.com>
2014-04-13 22:25:22 +0800

    commit to pull

---
+++ public_html/js/user-management.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#insert-submit").click(function(event) {
+        event.preventDefault();
+        insert_username = $("#insert-username").val();
+        insert_password = $("#insert-password").val();
+        insert_parent = $("#insert-parent").val();
+        $.ajax( {
+            
+        }
+    }
+
+});
