868d7a5bc642b37ab00a24e77a98f8b230558d1f
Tsenmu <i@tsenmu.com>
2014-04-13 21:14:12 +0800

    modified entry.php

--- public_html/logic/entry.php
+++ public_html/logic/entry.php
@@ -2,6 +2,7 @@
 session_start();
 define('BASE', substr(CWD, strlen($_SERVER['DOCUMENT_ROOT'])));
 define('PAGE', substr($_SERVER['SCRIPT_NAME'],1 + strlen(BASE)));
+require_once('database.php');
 if (!isset($_SESSION['current_user']))
 {
     header("Location: ../login.php");
@@ -9,9 +10,10 @@ if (!isset($_SESSION['current_user']))
 }
 else if (PAGE == 'index.php')
 {
+    $_SESSION['current_user_type'] = 1
     header("Location: ../home.php");
     exit;
 }
-require_once("database.php");
-//print_r(get_user_Info($_SESSION['current_user']));
+
+
 ?>
