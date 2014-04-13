254a4fd2229f907c197959ddebbe0af3731b2e62
Tsenmu <i@tsenmu.com>
2014-04-13 21:09:24 +0800

    add index.php redirection

--- public_html/logic/entry.php
+++ public_html/logic/entry.php
@@ -1,3 +1,17 @@
 <?php
 session_start();
+define('BASE', substr(CWD, strlen($_SERVER['DOCUMENT_ROOT'])));
+define('PAGE', substr($_SERVER['SCRIPT_NAME'],1 + strlen(BASE)));
+if (!isset($_SESSION['current_user']))
+{
+    header("Location: ../login.php");
+    exit;
+}
+else if (PAGE == 'index.php')
+{
+    header("Location: ../home.php");
+    exit;
+}
+require_once("database.php");
+//print_r(get_user_Info($_SESSION['current_user']));
 ?>
