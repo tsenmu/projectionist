46743baccf9d6314ff054f2d3250326bfa53f665
zhirui1992@gmail.com <xingouy@gmail.com>
2014-04-19 15:10:42 +0800

    add get_record_order_by_time

--- public_html/logic/control_record.php
+++ public_html/logic/control_record.php
@@ -129,5 +129,42 @@ function show_record($record_id)
 	return $record;
 }
 
+//-------------------multi array sort----------
+function multi_array_sort($multi_array, $sort_key, $sort=SORT_ASC)
+{
+	if(is_array($multi_array))
+	{
+		foreach($multi_array as $row_array)
+		{
+			if(is_array($row_array))
+			{
+				$key_array[]=$row_array[$sort_key];
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	else
+	{
+		return false;
+	}
+	array_multisort($key_array,$sort,$multi_array);
+	return $multi_array;
+}
+
+function get_record_order_by_time($user_id)
+{
+	$record=get_record($user_id);
+	if($record==NULL)
+	{
+		return "ERROR_RECORD_NULL";
+	}
+	$res=multi_array_sort($record,"date_time",SORT_ASC);
+	return $res;
+}
+
+
 	
 ?>
