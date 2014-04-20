8f1c8c37cdc60b64ecd4c6ec3269434abc952f54
zhirui1992@gmail.com <xingouy@gmail.com>
2014-04-20 16:54:57 +0800

    add search count

--- public_html/logic/test.php
+++ public_html/logic/test.php
@@ -24,7 +24,8 @@ require_once('control_record.php');
 //print_r(get_all_chain_info());
 //print_r(get_all_film_name());
 $res=search_record(1);
-
+print_r($res);
+echo "<br>";
 print_r(split_result($res,2,1));
 echo "<br>";
 print_r(split_result($res,2,2));
