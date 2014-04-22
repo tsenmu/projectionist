5aeb92a70e386d58c590dde5a1b2ddc43f907c03
Tsenmu <i@tsenmu.com>
2014-04-22 13:33:48 +0800

    switched to jquery1.*

--- public_html/includes/footer.inc.php
+++ public_html/includes/footer.inc.php
@@ -1,4 +1,4 @@
-<script src="js/jquery.min.js"></script>
+<script src="js/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/common.js"></script>
 <script src="js/navbar.js"></script>
